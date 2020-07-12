a9c7900e1b0bae24bb182072c0f19f041a8898ac
Sara M <sara@ursysconsulting.com>
2020-07-11 23:38:19 -0400

    fixed script, fixed playbook for testing

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,10 +2,14 @@
 
 echo "Hello World"
 #var=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/my-metadata -H "Metadata-Flavor: Google")
-curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/serverIp -H "Metadata-Flavor: Google" >> /etc/serverip.txt
+echo "check metadata download"
+sudo curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/serverIp -H "Metadata-Flavor: Google" >> /opt/serverip.txt
 #installing ansible
-sudo yum -y install epel-repo
-sudo yum -y update
-sudo yum install ansible -y
+echo "install epel-release"
+sudo yum -y install epel-release
+echo "install ansible"
+sudo yum -y install ansible 
+echo "downloads playbook"
 sudo curl https://raw.githubusercontent.com/UrsysC/terraform-gcp-pihole-ha/master/ansible/playbook.yml -o /opt/playbook.yml
+echo "run playbook"
 ansible-playbook /opt/playbook.yml
