eac90fb45ac54da238c0a2cff9ab10dc20083b06
Sara M <sara@ursysconsulting.com>
2020-07-11 23:21:10 -0400

    updated playbook

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,6 +4,8 @@ echo "Hello World"
 #var=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/my-metadata -H "Metadata-Flavor: Google")
 curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/serverIp -H "Metadata-Flavor: Google" >> /etc/serverip.txt
 #installing ansible
+sudo yum -y install epel-repo
+sudo yum -y update
 sudo yum install ansible -y
 sudo curl https://raw.githubusercontent.com/UrsysC/terraform-gcp-pihole-ha/master/ansible/playbook.yml -o /opt/playbook.yml
 ansible-playbook /opt/playbook.yml
